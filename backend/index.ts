b97832dde9184815c2da28cf6b874df735b666ae
Flinn Burgess <flinnburgess@gmail.com>
2023-02-16 19:38:18 +0000

    Install Prisma and Playwright

--- backend/index.ts
+++ backend/index.ts
@@ -1,21 +1,21 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import cors from 'cors'
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
-app.use(cors())
-const port = process.env.PORT
+const app = express();
+app.use(cors());
+const port = process.env.PORT;
 
-app.get('/', (_, res) => {
-    res.send('Express + Typescript Server')
-})
+app.get("/", (_, res) => {
+  res.send("Express + Typescript Server");
+});
 
-app.get('/test', (_, res) => {
-    res.send({data: 'bing bong'})
-})
+app.get("/test", (_, res) => {
+  res.send({ data: "bing bong" });
+});
 
 app.listen(port, () => {
-    console.log(`[server]: Server is running at http://localhost:${port}`)
-})
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
